57b54725e43a660f9fd1ce132aa8e2e1f44f8e86
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-05-06 00:56:03 +0530

    Added last name check

--- UserRegistraction.java
+++ UserRegistraction.java
@@ -15,11 +15,25 @@ class PatternCheck {
             System.out.println("The given input is not valid");
         }
     }
+
+public void testLastName() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Last Name");
+        String string = scan.nextLine();
+        test= Pattern.compile("^[A-Z]{1}[a-z]{2,}").matcher(string).matches();
+        if (test) {
+            System.out.println("The given input is valid");
+        }
+        else {
+            System.out.println("The given input is not valid");
+        }
+    }
 }
 
 public class UserRegistraction {
     public static void main(String[] args) {
         PatternCheck check = new PatternCheck();
         check.testFirstName();
+	check.testLastName();
     }
 }
