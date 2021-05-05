c170a66c07f179fa24e6573243d682471bb1178e
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-05-05 23:40:49 +0530

    Added first name check

---
+++ UserRegistraction.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+class PatternCheck {
+    boolean test;
+    public void testFirstName() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the First Name");
+        String string = scan.nextLine();
+        test= Pattern.compile("^[A-Z]{1}[a-z]{2,}").matcher(string).matches();
+        if (test) {
+            System.out.println("The given input is valid");
+        }
+        else {
+            System.out.println("The given input is not valid");
+        }
+    }
+}
+
+public class UserRegistraction {
+    public static void main(String[] args) {
+        PatternCheck check = new PatternCheck();
+        check.testFirstName();
+    }
+}
